da2b313c11bda22ae3a621b44dae2dd408b4bf62
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-08-20 15:40:48 -0400

    cowsay package

---
+++ python_class_02/packages.py
@@ -0,0 +1,11 @@
+
+import cowsay
+import sys
+
+""" if len(sys.argv) == 1:
+    cowsay.cow("Hello ", sys.argv[0]) """
+
+#cowsay.cow("Hello ", sys.argv[0])
+""" cowsay.cow("Hello " + sys.argv[0]) """
+
+cowsay.trex("Hello " + sys.argv[0])
