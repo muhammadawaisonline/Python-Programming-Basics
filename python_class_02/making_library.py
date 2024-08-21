4551f21b6658f75ad0056564e2e62f60b1913fcc
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-08-21 15:42:33 -0400

    Making Library

---
+++ python_class_02/making_library.py
@@ -0,0 +1,18 @@
+def main():
+    hello("world")
+    goodby("world")
+
+def hello(name):
+    print(f"Hello {name}")
+
+
+def goodby(name):
+    print(f"goodby {name}")
+
+
+
+## Conditionally calling main() function
+if __name__ == "__main__":
+    main()
+    
+    
---
+++ python_class_02/square_fn.py
@@ -0,0 +1,11 @@
+
+def square(n):
+    return n * n
+
+def main():
+    x = int(input("Enter intiger number "))
+
+    print(f"{square(x)} is square of {x} ")
+
+if __name__ == "__main__":
+    main()
