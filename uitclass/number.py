877b9feb096833ed7971fdf361947a0d5251682a
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-08-23 15:54:50 -0400

    Repeat Numbers in Puthon

---
+++ uitclass/number.py
@@ -0,0 +1,64 @@
+  #calculation
+#x = 1
+#y = 2
+#z = x + y
+#print(z)
+#print(x+y)
+
+
+   #User input Concatination (string as in put)
+#x = input("What's x? ") #3
+#y = input("Wht's y? ") #5
+#z = x + y 
+#print(z)
+   #answer 35
+
+   #converting sring into number
+#x = input("What's x? ") #3
+#y = input("Wht's y? ") #5
+#z = int(x) + int(y) 
+#print(z) #8
+   
+   
+   
+   #cleaning code
+#x = int(input("What's x? ")) 
+#y = int(input("Wht's y? ")) 
+#z = x + y 
+#print(z)
+#print(x + y)
+
+
+                  ##Float##
+#x = float(input("What's x? ")) #2.5
+#y = float(input("What's y? ")) #4.7
+#print(x + y) #7.2 
+#z = round(x + y)
+#print(z) #7
+    
+    
+                  ### USA number format
+#x = float(input("What's x? ")) #999
+#y = float(input("What's y? ")) #1
+
+#z = round(x + y) #1000
+#print(z) #1000
+                    ## Adding comma in long digit
+#print(f"{z:,}") #1,000
+
+
+
+                     #Division
+x = float(input("What's x? ")) 
+y = float(input("What's y? "))
+#z = x/y
+#print(z) #0.333333333
+
+#z = round(x/y, 2) 
+#print(z) #0.33
+
+z = x/y
+#print(round(z,2))
+    ##other way to round number
+print(f"{z:.2f}")
+
