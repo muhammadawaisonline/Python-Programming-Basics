5515089b4e127f0f524b82e1b953bc6c28417bd0
muhammad awais online <muhammadawaisonline00@gmail.com>
2024-08-24 16:02:40 -0400

    Repeat Conditionals and Conjunctions

---
+++ uitclass/conjunctions.py
@@ -0,0 +1,44 @@
+
+     #Conditionals
+""" score = int(input("Score: "))
+if score >= 90 and score <= 100:
+    print("Grad: A")
+elif score >= 80 and score <= 90:
+    print("Grad: B")
+elif score >= 70 and score <= 80:
+    print("Grad: C")
+elif score >= 60 and score <= 70:
+    print("Grad: D")
+else: 
+    print("Grad: F") """
+
+
+
+      #Making it more simpler
+""" score = int(input("Score: "))
+if score >= 90:
+    print("Grad: A")
+elif score >= 80:
+    print("Grad: B")
+elif score >= 70:
+    print("Grad: C")
+elif score >= 60:
+    print("Grad: D")
+else: 
+    print("Grad: F") """
+
+
+    #if
+score = int(input("Score: "))
+if score >= 90:
+    print("Grad: A")
+if score >= 80:
+    print("Grad: B")
+if score >= 70:
+    print("Grad: C")
+if score >= 60:
+    print("Grad: D")
+if score < 60: 
+    print("Grad: F")
+
+    
